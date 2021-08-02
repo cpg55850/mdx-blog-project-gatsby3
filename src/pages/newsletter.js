65e5bb67f1d2507bb2824fddf4bd8fb2d22c647c
Charles Graham <charlesgraham@charless-mbp.attlocal.net>
2021-08-01 22:54:36 -0500

    nevermind

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,47 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-
-const validationSchema = yup.object().shape({
-  name: yup.string().required('Required'),
-  email: yup.string().email('Invalid email format').required('Required'),
-})
-
 const NewsLetter = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validationSchema),
-  })
-
-  const encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&')
-  }
-
-  const onSubmit = (values, actions) => {
-    console.log(JSON.stringify(values, null, 2))
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact-form', ...values }),
-    })
-      .then(() => {
-        alert('Success')
-        actions.resetForm()
-      })
-      .catch(() => {
-        alert('Error')
-      })
-      .finally(() => actions.setSubmitting(false))
-  }
-
   return (
     <Layout>
       <section className="newsletter-page">
@@ -55,23 +15,22 @@ const NewsLetter = () => {
             netlify-honeypot="bot-field"
             data-netlify="true"
             action="/success"
-            onSubmit={handleSubmit(onSubmit)}
           >
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
 
             <input
-              {...register('name')}
+              type="text"
+              name="name"
               placeholder="Your name"
               className="form-control"
             />
-            <p>{errors.name?.message}</p>
             <input
-              {...register('email')}
+              type="email"
+              name="email"
               placeholder="Your email"
               className="form-control"
             />
-            <p>{errors.email?.message}</p>
             <button type="submit" className="btn form-control">
               subscribe
             </button>
